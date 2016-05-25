3d9a10e21343dabf59c57978bda60fd7e1a9c476
jegpub@gmail.com <jegpub@gmail.com>
2016-05-25 18:55:15 +0000

    Added Post CRUD ability

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,49 @@
+class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+  def index
+  end
+  
+  def new
+    @post = Post.new
+  end
+  
+  def create
+    @post = Post.new(post_params)
+    
+    if @post.save
+      redirect_to @post, notice: "Post Created!"
+    else
+      render 'new', notice: "Opps something went wrong! Try again!"
+    end
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @post.update(post_params)
+      redirect_to @post, notice: "Woohoo! Your article was saved!"
+    else
+      render 'edit', notice: "Not so fast. Something went haywire! Try again"
+    end
+  end
+  
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+  
+  
+  
+  private
+    def post_params
+      params.require(:post).permit(:title, :content)
+    end
+    
+    def find_post
+      @post = Post.find(params[:id])
+    end
+end
